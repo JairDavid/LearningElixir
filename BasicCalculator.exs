542e684d91dd06e35a395430b8af7160ef8f96cb
JairDavid <jairvasmar@gmail.com>
2021-07-21 23:51:48 -0500

    calculator added

---
+++ BasicCalculator.exs
@@ -0,0 +1,24 @@
+defmodule Calculate do
+  def sum(a,b) do
+    a+b
+  end
+  def res(a,b) do
+    a-b
+  end
+  #Using guard: guard evaluates the condition of the given argument, the order matters
+  #Underscore: the undescore ignores the given argument
+  def div(a,b) when a==0 and b==0 do
+    0
+  end
+
+  def div(_a,b) when b==0 do
+    :infinite
+  end
+
+  def div(a,b) do
+    a/b
+  end
+  def mul(a,b) do
+    a*b
+  end
+end
